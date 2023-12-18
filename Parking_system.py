e2032f714722fccb4da16f4b59a8d14612456c2f
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2023-12-18 12:41:00 -0700

    changed to accurately reflect current date

--- Parking_system.py
+++ Parking_system.py
@@ -1,4 +1,12 @@
+import datetime
 
+xTime = datetime.datetime.now()
+date_year = xTime.year 
+date_month= xTime.month
+date_day=xTime.day
+
+
+1
 space_avail = 4
 registered_plates=[]
 registered_CC=[]
@@ -18,31 +26,37 @@ def register_vehicle1():
             #print (registered_CC) uncomment to see a printed list of the credit card added to list registered_CC
             space_avail -= 1
            # print (space_avail) uncomment this line if you would like to see the remaining parking spaces
-            restart3 = input("Please Press enter to continue...")
+            input("Please Press enter to continue...")
         else:
             print (f"The vehicle with plate {plate_number} is registered in the lot.")
-            restart4 = input("Please Press enter to continue...")
+            input("Please Press enter to continue...")
     else:
         print ("The parking lot is full.")
-        restart5 = input("Please Press enter to continue...")
+        input("Please Press enter to continue...")
     
 def verify_vehicle2(verify):
     if verify in registered_plates:
         print (f"The vehicle with plate# {verify} is registered in the lot.")
-        restart6 = input("Please Press enter to continue...")
+        input("Please Press enter to continue...")
     else:
         print ("The vehicle is NOT registered.")
-        restart7 = input("Please Press enter to continue...")
+        input("Please Press enter to continue...")
+
+
+
+
+
+
 
 def display_vehicle3():
     # if len(registered_plates) == 0: can reverse the is statment to apply if statment on an empty list
     if not registered_plates:
         print ("The Parking Lot is empty!")
-        restart9 = input("Please Press enter to continue...") # allows the user a break
+        input("Please Press enter to continue...") # allows the user a break
     else:
         my_file = open('vehicle.txt', 'w') # opens the file in write mode, if nothing is written(i think if nothing is written in it will only delete it) in pyhton deletes all contents in the file. this ensures we have a blank document before we write to it.
         my_file.close()
-        print('A list of registered vehicles for 2023-10-29') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
+        print(f'A list of registered vehicles for {date_day}-{date_month}-{date_year}') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
         my_file = open('vehicle.txt', 'a')
         my_file.write("====================\n")
         my_file.write ("       Plate\n")
@@ -51,14 +65,14 @@ def display_vehicle3():
             my_file.write(f"{str(item):>11}\n") # to format data types string you must specify str before the variable example{str(item):>11.2f} ////////  {total_interest:>14.2f} is meant to format integers.
         my_file.write("====================\n")
         my_file.close()
-        print('Daily parking summary for 2023-10-29') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
+        print(f'Daily parking summary for {date_day}-{date_month}-{date_year} ') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
         my_file= open('vehicle.txt', 'r')
     # print each line of file
         for each_line in my_file:
             each_line = each_line.rstrip()
             print(each_line)
         my_file.close()
-        restart8 = input("Please Press enter to continue...") # allows the user a break
+        input("Please Press enter to continue...") # allows the user a break
 
 def display_Charges4():
     my_file = open('charges.txt', 'w') # opens the file in write mode, if nothing is written(i think if nothing is written in it will only delete it) in pyhton deletes all contents in the file. this ensures we have a blank document before we write to it.
@@ -73,19 +87,19 @@ def display_Charges4():
     for i in range(len(registered_plates)):
         plate = registered_plates[i]
         cc = registered_CC[i]
-        my_file.write(f"{str(plate):>11}{str(cc):>22}{charge:>16}\n")# to format data types string you must specify str before the variable example{str(item):>11.2f} ////////  {total_interest:>14.2f} is meant to format integers.
+        my_file.write(f"{str(plate):>12}{str(cc):>22}{charge:>15}\n")# to format data types string you must specify str before the variable example{str(item):>11.2f} ////////  {total_interest:>14.2f} is meant to format integers.
 
     my_file.write("======================================================\n")
     my_file.write(f"{'Total':>12}{total_charges_for_day:>37}") # you dont have to press space 12 times for proper spacing you can put a string into {} curcly braces and use the :>13 format option. just make sure to not use "" if you used them to surround your original statment.
     my_file.close()
-    print('Daily parking summary for 2023-10-29') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
+    print(f'Daily parking summary for {date_day}-{date_month}-{date_year}') # Can we get a function/obj/class(whatever) to display the date for that day? or should this be tied to the date the text file was created or last edited?
     my_file= open('charges.txt', 'r')
 # print each line of file
     for each_line in my_file:
         each_line = each_line.rstrip()
         print(each_line)
     my_file.close()
-    restart10 = input("Please Press enter to continue...") # allows the user a break
+    input("Please Press enter to continue...") # allows the user a break
 
 def remove_vehicle5(remove_plate):
     global space_avail # declare a global variable inside the function
@@ -95,10 +109,10 @@ def remove_vehicle5(remove_plate):
         del registered_CC[index_number] # deletes the index number of the credit card from the list resitered_CC
         print (f"{remove_plate} is removed.")
         space_avail += 1
-        restart11 = input("Please Press enter to continue...") # allows the user a break
+        input("Please Press enter to continue...") # allows the user a break
     else:
         print (f"{remove_plate} is not registered.")
-        restart12 = input("Please Press enter to continue...") # allows the user a break
+        input("Please Press enter to continue...") # allows the user a break
 
 def clear_vehicle6():
     global space_avail  
@@ -110,7 +124,7 @@ def clear_vehicle6():
     registered_plates.clear()
     registered_CC.clear()
     print ("All vehicles were removed and the lot is empty.")
-    restart13 = input("Please Press enter to continue...") # allows the user a break
+    input("Please Press enter to continue...") # allows the user a break
     #my_list = [] both of these options will clear a list
     #my_list.clear() both of these options will clear a list
   
@@ -121,7 +135,7 @@ def check_password():
         return authorize 
     else:
         print("Password is incorrect!")
-        restart2 = input("Please Press enter to continue...")
+        input("Please Press enter to continue...")
         authorize = False
         return authorize
 
@@ -160,8 +174,8 @@ def print_menu():
                 display_Charges4()
         elif menu_option == "5":
             authorize = check_password()
-            check_plate = input("Enter your plate number:")
             if authorize == True:
+                check_plate = input("Enter your plate number:")
                 remove_vehicle5(check_plate)
         elif menu_option == "6":
             authorize = check_password()
@@ -171,9 +185,7 @@ def print_menu():
             i = False
         else:
             print ("Invalid input")
-            restart = input("Please Press enter to continue...")
+            input("Please Press enter to continue...")
 
 
 print_menu()
-
-#this is a test 
